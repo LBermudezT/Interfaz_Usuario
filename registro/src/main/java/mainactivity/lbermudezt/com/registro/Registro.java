33f064adc3fff4df85ea71a66d4aa2031955dad2
Luciano Bermudez <Luciano Bermudez>
2016-03-29 13:12:49 -0500

    Initial commit

---
+++ registro/src/main/java/mainactivity/lbermudezt/com/registro/Registro.java
@@ -0,0 +1,26 @@
+package mainactivity.lbermudezt.com.registro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+    }
+}
+public class Ciudades extends String[] = { "Medellin", "Bogota", "Cali",
+        "Barranquilla", "Cartagena", "Santa Marta", "Armenia", "Buga", "Valledupar", "Sincelejo" };
+<resources>
+<string-array name="Ciudades">
+<item>Medellin</item>
+<item>Bogota</item>
+<item>Cali</item>
+<item>Barranquilla</item>
+<item>Cartagena</item>
+<item>Santa Marta</item>
+</string-array>
+</resources>
